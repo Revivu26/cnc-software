c162e28061c382c00f73cb85a3a566d50dac9568
lanDo403 <104690289+lanDo403@users.noreply.github.com>
2025-12-10 21:52:30 +0300

    Лыков Иван Андреевич: Добавил прототип интерфейса

---
+++ src/main.py
@@ -0,0 +1,44 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtSerialPort import QSerialPortInfo
+from PyQt5 import uic, QtGui
+import sys
+#import serial.tools.list_ports as get_list
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = uic.loadUi('mainwindow.ui', self)
+        self.mainTabs.setCurrentIndex(1)
+        self.GetCOMPorts()
+
+    def GetCOMPorts(self):
+        self.getPorts = QSerialPortInfo()
+        ports = list(self.getPorts.availablePorts())
+        if ports:
+            self.Append('<<<<ПОРТЫ НАЙДЕНЫ>>>>\n')
+            self.Append('Список портов:\n')
+            for port in ports:
+                self.portsComboBox.addItem(port.portName())
+                self.Append(port.portName())
+            self.Append('\n<<<<ВЫПАДАЮЩИЙ СПИСОК ЗАПОЛНЕН ВСЕМИ ДОСТУПНЫМИ ПОРТАМИ>>>>\n')
+        else:
+            self.Append('<<<<ПОРТЫ НЕ ОБНАРУЖЕНЫ>>>>\n')
+
+
+    def Append(self, text):
+        cursor = self.consoleEdit.textCursor()
+        cursor.movePosition(cursor.End)
+        cursor.insertText(text)
+        self.consoleEdit.moveCursor(QtGui.QTextCursor.End)
+
+if __name__ == "__main__":
+    if sys.platform.startswith('win'):
+        current_encoding = 'cp1251'
+    else:
+        current_encoding = 'utf8'
+
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    sys.exit(app.exec_())
